c5033b7ea0c0a11912b967341052f8576bddb663
Yash Urade <57934915+yashurade27@users.noreply.github.com>
2025-06-28 20:08:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
   return (
-    <div className="container">
-      <div className="logos">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Demo for Vercel-Clone</h1>
-    </div>
-  )
+    <h1>Demo for Vercel-Clone</h1>
+  );
 }
 
 export default App;
